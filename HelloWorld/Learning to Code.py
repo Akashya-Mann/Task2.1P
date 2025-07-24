dfea7033a23e958a80dcb268893f814facf44bbd
Akashya-Mann <s224597443@deakin.edu.au>
2025-07-24 14:56:26 +1000

    Add files via upload

---
+++ HelloWorld/Learning to Code.py
@@ -0,0 +1,67 @@
+#First_name = "Akashya "
+#full_name = First_name + Last_Name
+#print("Hello "+ full_name)
+#age = 17 Integer
+#age += 1
+#print("Your Age is:"+str(age))
+#print(type(age))
+#height = 182.2 Float
+#print("Your Height is:"+ " "+str(height) + "cm")
+#Human = True # This is called a Boolean usful in if statements
+#print("Are you a very intelligent human?: " + str(Human))
+# 4 basic Data types:
+# Strings which store a series of characters
+# Int which are whole numbers
+# Floats Decimal Numbers
+
+# first_name = "Akashya"
+# last_name = "Partap Mann"
+# print("Hello " +first_name + " " + last_name)
+# Age = 17
+# Age += 1
+# print("Your Age is:" + " " + str(Age))
+# Human = True
+# print("and you are a " +str(Human) + " " + "Human")
+
+# Multiple Assignment allows us to assign multiple variables using one line of code
+# I, Know, You = "I", "do", True
+# print(I), print(Know), print(You)
+# Spongebob = Patrick = Sandy = Squidward = 30
+# print(Spongebob)
+# name = "Akashya"
+# print(len(name))
+# print(name.find("h"))
+# print(name.capitalize())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.replace("Akashya","Akashya Partap Mann"))
+# print(name*1)
+# print(name.count("a"))
+
+# Type Casting = Convert the data type of value to another data type
+# x = 2
+# y = ("3")
+# z = 2.0
+# print(x, y, z)
+# print(x)
+# print(y)
+# print(int(z))
+# y = int(y)
+# print(y*4)
+# print(y*3)
+# print("X and Y are: " + str(y))
+
+name = input("What is your name?")
+age = int(input("How old were you last year?"))
+print("Hello "+ name)
+age = age + 1
+input("Are you Sure?")
+Human = True
+input("Are you Human?")
+input("You are very intelligent Human "+str(Human))
+print("Akashya You are " + "" + str( age)+ " Years old,This year")
+height = float(input("How Tall are you?"))
+height = height + 160
+print("Ahh I see "+ str(height)+ " Is a very good height you must be very proud "), input("Are you proud?")
+print("Hello "+ name + "You are "+ str(age) + " years old")
+print("and "+ str(height) + " cm tall")
